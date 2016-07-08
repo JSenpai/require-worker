794c5a0d8e941500262ea72ac0802ce7f9e19f6a
Unchosen <unchosen.aus@gmail.com>
2016-07-08 19:43:49 +0800

    More examples for new features

--- examples/example2.js
+++ examples/example2.js
@@ -4,6 +4,7 @@
 // Note: For internal modules, wrapRequire:true must be used as a require option
 // Some of these examples are from the Nodejs documentation
 var rw = require('../index.js');
+rw.verboseIO = false;
 
 // The OS module
 var osWorker = rw.require('os',{ wrapRequire:true }), os = osWorker.methods;
@@ -23,5 +24,31 @@ path.normalize('/foo/bar//baz/asdf/quux/..').then(function(result){
 	pathWorker.kill();
 });
 
-// require-worker is *currently* incompatible with the modules: http, crypto, eventemitter and etc, since the functions return objects with other functions on them.
-// * one day..
+// The Events module
+var eventsWorker = rw.require('events',{ wrapRequire:true });
+var newEventEmitterPromise = eventsWorker.call(null,eventsWorker.callOptionsObject({ newSelf:true }));
+newEventEmitterPromise.then(function(result){
+	var events = result.methods;
+	events.once('test',function(arg1,arg2){ // This callback is not triggering?
+		console.log('eventEmitter .once .emit - test:',arg1,arg2);
+		eventsWorker.kill();
+	},eventsWorker.callOptionsObject({ ignoreResult:true })).then(function(){});
+	events.emit('test','foo','bar',eventsWorker.callOptionsObject({ ignoreResult:true })).then(function(){});
+});
+
+// The Crypto module
+// Not yet implemented: requires work on normal object handling
+/*
+var cryptoWorker = rw.require('crypto',{ wrapRequire:true }), crypto = cryptoWorker.methods;
+crypto.createHmac('sha256','abcdefg',cryptoWorker.callOptionsObject({ useReturnOnly:null })).then(function(c){
+	console.log('crypto.createHash');
+	return c.update('I love cupcakes');
+}).then(function(c){
+	console.log('crypto.createHash .update');
+	return c.digest('hex');
+}).then(function(result){
+	console.log('crypto.createHash .update .digest');
+	console.log('crypto result:',result);
+	pathWorker.kill();
+});
+*/
