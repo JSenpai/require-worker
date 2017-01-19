edea2d315952694df853a09b11d437a51b3c63c1
Unchosen <unchosen.aus@gmail.com>
2017-01-19 13:58:24 +0800

    More work

--- examples/example1_module.js
+++ examples/example1_module.js
@@ -6,3 +6,10 @@ exports.foo = 'bar';
 exports.hello = (arg)=>{
 	return 'Hello '+arg+'!';
 };
+
+exports.someObject = {
+	name: 'Tree',
+	type: 'Oak',
+	age: '25y7m4d',
+	height: '6.8m'
+};
--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 const path = require('path');
 const childProcess = require('child_process');
 const eventEmitter = require('events');
-const _ = require('underscore');
 
+const _ = require(path.resolve(__dirname,'./lib/underscore-with-mixins'));
 const proxyCom = require(path.resolve(__dirname,'./lib/proxy-communication'));
 const ipcTransport = require(path.resolve(__dirname,'./lib/ipc-transport'));
 
-module.exports = (target)=>{
-	if(_.isString(target))try{ target = path.resolve(target); }catch(err){}
+module.exports = exports = (target)=>{
+	if(_.isString(target))try{ target = require.resolve(target); }catch(err){}
 	if(hostsMap.has(target)) return hostsMap.get(target);
 	if(clientsMap.size===0 && hostsMap.size>0) throw Error("first argument must be a valid require-worker host or file path");
 	if(!clientsMap.has(target)) throw Error("first argument must be a valid require-worker client or file path");
@@ -19,7 +19,7 @@ module.exports = (target)=>{
 
 module.exports.require = (path,options)=>new client(path,options);
 
-var getStackFiles = function getStackFiles(){
+const getStackFiles = function getStackFiles(){
 	let opst = Error.prepareStackTrace, thisError, result = [];
 	Error.prepareStackTrace = (errStackStr,cssfArr)=>cssfArr;
 	thisError = new Error();
@@ -28,18 +28,16 @@ var getStackFiles = function getStackFiles(){
 	Error.prepareStackTrace = opst;
 	for(let i=0,l=cssfArr.length; i<l; i++){
 		let cssf = cssfArr[i]; // https://github.com/v8/v8/wiki/Stack-Trace-API
-		let file=cssf.getFileName();
+		let file = cssf.getFileName();
 		if(file===__filename) continue;
-		//let type=cssf.getTypeName(), fn=cssf.getFunctionName();
-		//if(file==='module.js' && (type==='Module' || type==='Object' || type==='Function' || type===null) && fn.indexOf('Module')!==-1) continue;
-		//if(file==='bootstrap_node.js' && type===null && fn==='run') continue;
 		result.push(file);
 	}
 	return result; 
 };
 
-var clientsMap = new Map(), clientIndex = 0;
-var client = function requireWorkerClient(file,options={ ownProcess:false, shareProcess:null }){
+var clientIndex = 0;
+const clientsMap = new Map();
+const client = function requireWorkerClient(file,options={ ownProcess:false, shareProcess:false, parentModule:false }){
 	if(!_.isString(file)) throw Error("first argument must be a string (require path)");
 	if(!_.isObject(options)) throw Error("second argument must be an object (options) or undefined");
 	var self = this;
@@ -56,22 +54,23 @@ var client = function requireWorkerClient(file,options={ ownProcess:false, share
 	self.file = file;
 	if(path.isAbsolute(file)) hostOptions.file = file;
 	else {
-		try{
+		if(options.parentModule && options.parentModule.require) try{ hostOptions.file = options.parentModule.require.resolve(file); }catch(err){}
+		if(!hostOptions.file) try{
 			var prevStackDir = path.dirname(getStackFiles()[0]);
 			try{ // file relative to the directory
 				hostOptions.file = require.resolve(path.resolve(prevStackDir,file));
 			}catch(err1){
-				try{ // file itself (eg, nodejs module)
+				try{ // file itself (eg, nodejs module), on local require
 					hostOptions.file = require.resolve(file);
 				}catch(err2){ // fallback to setting cwd of fork
 					hostOptions.file = file;
+					if(!('forkOptions' in options)) options.forkOptions = {};
 					options.forkOptions.cwd = prevStackDir;
 					options.ownProcess = true;
 				}
 			}
-		}catch(err3){
-			hostOptions.file = file;
-		}
+		}catch(err){}
+		if(!hostOptions.file) hostOptions.file = file;
 	}
 	self.hostOptions = hostOptions;
 	var rwPObj = rwProcess({ client:self });
@@ -81,7 +80,8 @@ var client = function requireWorkerClient(file,options={ ownProcess:false, share
 		rwPTransport.send('requireHost',hostOptions);
 	});
 	rwPTransport.send('processReady?');
-	try{ clientsMap.set(path.resolve(self.file),self); }catch(err){}
+	if(!clientsMap.has(file)) clientsMap.set(file,self);
+	try{ clientsMap.set(hostOptions.file,self); }catch(err){}
 	self.proxyCom = proxyCom.create({
 		transport: { type:'ipcTransport', instance:self.ipcTransport },
 		requireWorkerClient: self
@@ -104,43 +104,43 @@ client.prototype = {
 	setChildReferenced: function(bool){
 		if(bool) this.child.ref();
 		else this.child.unref();
-	},
-	hasProperty: function(property){
-		
-	},
-	deleteProperty: function(property){
-		
-	},
-	setProperty: function(property,value){
-		
-	},
-	getProperty: function(property){
-		// use case: requireWorker(test).getProperty('foo').then((value)=>{ /* value='bar' */ });
-		// use case: requireWorker(test).getProperty('someObject').then((value)=>{ /* value=ProxyInterface */ });
 	}
 };
 
-var rwProcessMap = new Map(), rwProcessIndex = 0;
-var rwProcess = function(options={}){
+var rwProcessIndex = 0;
+const rwProcessMap = new Map();
+const rwProcess = function(options={}){
 	var client = options.client;
 	var ownProcess = !!client.options.ownProcess;
+	var shareProcess = client.options.shareProcess;
 	if(!client) return Promise.reject();
 	var createNewProcess = (rwProcessMap.size===0 || ownProcess);
 	var useExistingObj = null;
 	if(!createNewProcess){
 		createNewProcess = true;
 		for(var [child,obj] of rwProcessMap){
-			if(!obj.ownProcess){
+			if(shareProcess && (shareProcess===obj.client || shareProcess===obj.client.proxy || shareProcess===child)){
+				createNewProcess = false;
+				useExistingObj = obj;
+				break;
+			}
+			if(!shareProcess && !obj.ownProcess){
 				createNewProcess = false;
 				useExistingObj = obj;
 				break;
 			}
 		}
+		if(shareProcess && !useExistingObj) throw Error("Existing require-worker process could not be found, set shareProcess to a client object, client proxy, or a process child");
 	}
 	if(createNewProcess){
-		var rwPObj = { ownProcess, id:'require-worker:process:'+(++rwProcessIndex)+':'+Date.now() };
+		var rwPObj = { ownProcess, id:'require-worker:process:'+(++rwProcessIndex)+':'+Date.now(), client };
 		rwPObj.ipcTransport = ipcTransport.create({ id:rwPObj.id });
+		if(!('forkOptions' in client.options)) client.options.forkOptions = {};
+		if(!('cwd' in client.options.forkOptions)) client.options.forkOptions.cwd = process.cwd();
+		//var processArgv = _.clone(process.execArgv);
+		//if(process.execArgv.indexOf('--inspect')!==-1) process.execArgv.splice(process.execArgv.indexOf('--inspect'),1);
 		rwPObj.child = client.child = childProcess.fork(__filename,['--rwProcess',rwPObj.id],client.options.forkOptions);
+		//process.execArgv = processArgv;
 		rwPObj.ipcTransport.setChild(rwPObj.child);
 		rwProcessMap.set(rwPObj.child,rwPObj);
 		return rwPObj;
@@ -150,7 +150,7 @@ var rwProcess = function(options={}){
 	}
 };
 
-var checkNewProcess = ()=>{
+const checkNewProcess = ()=>{
 	if(require.main===module && process.argv.length===4 && process.argv[2]==='--rwProcess'){
 		var ipcTransportID = process.argv[3];
 		var transport = ipcTransport.create({
@@ -168,8 +168,8 @@ var checkNewProcess = ()=>{
 	}
 };
 
-var hostsMap = new Map();
-var host = function requireWorkerHost({ transport, ipcTransportID, file }){
+const hostsMap = new Map();
+const host = function requireWorkerHost({ transport, ipcTransportID, file }){
 	var self = this;
 	if(transport!=='ipcTransport') throw Error("Invalid transport, only ipcTransport is currently implemented");
 	self.events = new eventEmitter();
@@ -190,7 +190,7 @@ var host = function requireWorkerHost({ transport, ipcTransportID, file }){
 			self.proxyCom.setProxyTarget(self.exports);
 		}
 	});
-	try{ hostsMap.set(path.resolve(file),self); }catch(err){}
+	try{ hostsMap.set(require.resolve(file),self); }catch(err){}
 	try{
 		self.exports = require(file);
 		hostsMap.set(self.exports,self);
