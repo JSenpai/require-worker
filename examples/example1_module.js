c440daf12fe5d008922e4960661cda27a9e0ed9b
Unchosen <unchosen.aus@gmail.com>
2017-02-10 23:37:38 +0800

    Clear outdated examples

--- examples/example1_module.js
+++ examples/example1_module.js
@@ -1,16 +1,4 @@
 /* global exports */
 "use strict";
 
-exports.foo = 'bar';
-
-exports.hello = (arg)=>{
-	//require('../')(exports)._destroy();
-	return 'Hello '+arg+'!';
-};
-
-exports.someObject = {
-	name: 'Tree',
-	type: 'Oak',
-	age: '25y7m4d',
-	height: '6.8m'
-};
+// todo
