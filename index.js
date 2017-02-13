5855b7fb5f9138b850afa0980fa8eb2d63dc8495
Unchosen <unchosen.aus@gmail.com>
2017-02-13 22:48:29 +0800

    Move process manager into core-process-manager file

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 "use strict";
 
 const path = require('path');
-const childProcess = require('child_process');
-const eventEmitter = require('events');
 
 const _ = require(path.resolve(__dirname,'./lib/underscore-with-mixins'));
-const proxyCom = require(path.resolve(__dirname,'./lib/proxy-communication'));
 const ipcTransport = require(path.resolve(__dirname,'./lib/ipc-transport'));
 const coreClient = require(path.resolve(__dirname,'./lib/core-client'));
 const coreHost = require(path.resolve(__dirname,'./lib/core-host'));
+const coreProcessManager = require(path.resolve(__dirname,'./lib/core-process-manager'));
 
 module.exports = exports = (target)=>{
 	if(_.isObject(target) && 'constructor' in target && 'client' in target.constructor && target.constructor.client instanceof coreClient.requireWorkerClient){
@@ -22,11 +20,19 @@ module.exports = exports = (target)=>{
 	return coreClient.clientsMap.get(target);
 };
 
-var requireWorkerObj = { exports, coreClient, coreHost };
+var requireWorkerObj = { exports, __filename, coreClient, coreHost, coreProcessManager };
+
 coreClient.setRequireWorker(requireWorkerObj);
 exports.coreClient = coreClient;
+
 coreHost.setRequireWorker(requireWorkerObj);
 exports.coreHost = coreHost;
+
+coreProcessManager.setRequireWorker(requireWorkerObj);
+exports.coreProcessManager = coreProcessManager;
+exports.prepareProcesses = coreProcessManager.prepareProcesses;
+exports.destroyPreparedProcesses = coreProcessManager.destroyPreparedProcesses;
+exports.getPreparedProcessesCount = coreProcessManager.getPreparedProcessesCount;
 
 exports.require = (path,options)=>new coreClient.requireWorkerClient(path,options);
 
@@ -78,93 +84,6 @@ exports.preConfiguredProxy = (target,options)=>{
 	}
 	if(!interfaceObj || !client) throw Error("Target not found");
 	return client.proxyCom.createMainProxyInterface(_.deepExtend(_.deepExtend({},interfaceObj.options),{ preConfigure:options }));
-};
-
-const rwPreparedProcessMap = new Map();
-exports.getPreparedProcessesCount = ()=>{
-	return rwPreparedProcessMap.size;
-};
-exports.prepareProcesses = (options={ count:1, forkOptions:{} })=>{
-	for(var i=0,l=options.count; i<l; i++){
-		var rwPObj = rwCreateProcess(_.omit(options,['count']));
-		rwPObj.preparedProcess = true;
-		rwPreparedProcessMap.set(rwPObj.child,rwPObj);
-	}
-	return true;
-};
-exports.destroyPreparedProcesses = ()=>{
-	for(var [key,obj] of rwPreparedProcessMap){
-		obj.child.unref();
-		obj.child.kill();
-		rwPreparedProcessMap.delete(key);
-	}
-	return true;
-};
-var rwProcessIndex = 0;
-const rwCreateProcess = (options={ forkOptions:{} })=>{
-	var rwPObj = { id:'require-worker:process:'+(++rwProcessIndex)+':'+Date.now() };
-	rwPObj.ipcTransport = ipcTransport.create({ id:rwPObj.id });
-	if(!('forkOptions' in options)) options.forkOptions = {};
-	if(!('cwd' in options.forkOptions)) options.forkOptions.cwd = process.cwd();
-	//var processArgv = _.clone(process.execArgv);
-	//if(process.execArgv.indexOf('--inspect')!==-1) process.execArgv.splice(process.execArgv.indexOf('--inspect'),1);
-	rwPObj.child = childProcess.fork(__filename,['--rwProcess',rwPObj.id],options.forkOptions);
-	//process.execArgv = processArgv;
-	rwPObj.ipcTransport.setChild(rwPObj.child);
-	return rwPObj;
-};
-
-const rwProcessMap = requireWorkerObj.rwProcessMap = new Map();
-const rwProcess = requireWorkerObj.rwProcess = (options={})=>{
-	var client = options.client;
-	var ownProcess = !!client.options.ownProcess;
-	var shareProcess = client.options.shareProcess;
-	if(!client) return Promise.reject();
-	var createNewProcess = (rwProcessMap.size===0 || ownProcess);
-	var useExistingObj = null;
-	if(!createNewProcess){
-		createNewProcess = true;
-		for(var [key,obj] of rwProcessMap){
-			if(shareProcess && (shareProcess===obj.client || shareProcess===obj.client.proxy || shareProcess===key)){
-				createNewProcess = false;
-				useExistingObj = obj;
-				break;
-			}
-			if(!shareProcess && !obj.ownProcess){
-				createNewProcess = false;
-				useExistingObj = obj;
-				break;
-			}
-		}
-		if(shareProcess && !useExistingObj) throw Error("Existing require-worker process could not be found, set shareProcess to a client object, client proxy, or a process child");
-	}
-	if(createNewProcess){
-		var rwPObj, preparedProcess = false;
-		if(!('forkOptions' in client.options)) client.options.forkOptions = {};
-		if(!('cwd' in client.options.forkOptions)){
-			for(var [key,obj] of rwPreparedProcessMap){
-				if(obj.preparedProcess && !preparedProcess){
-					preparedProcess = obj;
-					rwPreparedProcessMap.delete(key);
-					break;
-				}
-			}
-		}
-		if(preparedProcess) rwPObj = preparedProcess;
-		else rwPObj = rwCreateProcess({ forkOptions:client.options.forkOptions });
-		rwPObj.ownProcess = ownProcess;
-		rwPObj.client = client;
-		client.child = rwPObj.child;
-		rwProcessMap.set(rwPObj.child,rwPObj);
-		rwProcessMap.set(client,rwPObj);
-		return rwPObj;
-	} else {
-		var rwPObj = _.clone(useExistingObj);
-		rwPObj.client = client;
-		client.child = rwPObj.child;
-		rwProcessMap.set(client,rwPObj);
-		return rwPObj;
-	}
 };
 
 const checkNewProcess = ()=>{
